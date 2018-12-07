6cfc8862500ebb3a7c1281426ab1c96f5dab4aae
liguanglei <liguanglei@boyue.com>
2018-12-07 11:48:07 +0800

    Initial Commit

---
+++ GlVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GlVideoPlayer
+//
+//  Created by 小柠檬 on 2018/12/7.
+//  Copyright © 2018年 gleeeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
